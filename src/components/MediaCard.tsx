da8252855e93a8b7913e2814637db0099858582d
Serhat Sergikaya <serhatsergikaya@gmail.com>
2024-05-08 02:12:30 +0300

    Add rating badge to the media card

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -1,6 +1,7 @@
 import { Movie } from "../hooks/useMovies";
 import Moment from "moment";
 import {
+  Badge,
   Card,
   CardBody,
   CardFooter,
@@ -19,14 +20,19 @@ const MediaCard = ({ movie }: Props) => {
   const image_url = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
 
   const date = Moment(movie.release_date).format("MMM YY");
+
+  const rating = Number(movie.vote_average).toFixed(1);
   return (
     <Card borderRadius={20} overflow="hidden">
       <Image src={image_url} />
       <CardBody>
         <Heading fontSize="2xl">{movie.title}</Heading>
       </CardBody>
-      <HStack>
+      <HStack marginX={3} marginBottom={2} justifyContent="space-between">
         <Text>{date}</Text>
+        <Badge variant="subtle" fontSize={20} colorScheme="green">
+          {rating}
+        </Badge>
       </HStack>
     </Card>
   );
